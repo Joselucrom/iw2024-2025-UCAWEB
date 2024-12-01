9def41449de5c4f471de8702393721c56574e0f2
Javi.Rabago <javi.rabamonte@alum.uca.es>
2024-12-01 18:45:35 +0100

    modificar usuarios como admin

--- src/main/java/uca/es/iw/data/UserRepository.java
+++ src/main/java/uca/es/iw/data/UserRepository.java
@@ -1,16 +1,20 @@
 package uca.es.iw.data;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
 
     Optional<User> findByUsername(String username);
 
-    //List<User> findByUsernameContainingAndEmailContainingAndRoles_NameContaining(String username, String email, Role role);
-
-    //List<User> findByUsernameContainingAndEmailContaining(String username, String email);
+    /*@Transactional
+    @Modifying
+    @Query(value = "DELETE FROM user_roles WHERE user_id = :userId", nativeQuery = true)
+    void deleteRolesByUserId(@Param("userId") Long userId);*/
 }
