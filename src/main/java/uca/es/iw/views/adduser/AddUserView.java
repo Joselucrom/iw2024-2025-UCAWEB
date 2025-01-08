e398db6930294dd5fba6caf86793df686cea8cca
alberto-1309 <alberto.melladomontes@gmail.com>
2025-01-08 20:25:35 +0100

    Multilenguaje en PromotorView

--- src/main/java/uca/es/iw/views/adduser/AddUserView.java
+++ src/main/java/uca/es/iw/views/adduser/AddUserView.java
@@ -14,7 +14,6 @@ import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Menu;
-import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import jakarta.annotation.security.RolesAllowed;
 import org.springframework.beans.factory.annotation.Autowired;
